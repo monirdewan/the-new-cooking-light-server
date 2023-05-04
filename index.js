37e33a0ae4e2384d0ba7086038a01259713810ce
Monir <hossinmonir681@gmail.com>
2023-05-04 12:09:00 +0600

    data loaded successful

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 5000;
+const recipes = require('./data/recipes.json')
+
+app.get('/',(req,res)=>{
+    res.send("The new cooking light server is running");
+})
+
+app.get('/recipes',(req, res)=>{
+    res.send(recipes)
+})
 
 
 
