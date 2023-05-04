f03d8faaea7ef004ef00031fe154dcfb107428ba
Monir <hossinmonir681@gmail.com>
2023-05-04 12:11:23 +0600

    cors inastall and setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+var cors = require('cors')
 const app = express();
-
+app.use(cors())
 const port = process.env.PORT || 5000;
 const recipes = require('./data/recipes.json')
 
